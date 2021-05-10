9c46d6bf2e3a20d43fcc3d8258fd466fc318538f
Ali Sanhaji <ali.sanhaji@wearelog.fr>
2021-05-10 15:04:37 +0200

    Adapt terraform file to newer versions

--- terraform/cluster-infra.tf
+++ terraform/cluster-infra.tf
@@ -1,17 +1,27 @@
+terraform {
+  required_version = ">=0.14"
+  required_providers {
+    libvirt = {
+      source  = "multani/libvirt"
+      version = "0.6.3-1+4"
+    }
+  }
+}
+
 provider "libvirt" {
   uri = "qemu:///system"
 }
 
-resource "libvirt_volume" "ubuntu1804_cloud" {
-  name = "ubuntu18.04.qcow2"
+resource "libvirt_volume" "ubuntu2004_cloud" {
+  name = "ubuntu20.04.qcow2"
   pool = "default"
-  source = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
+  source = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
   format = "qcow2"
 }
 
-resource "libvirt_volume" "ubuntu1804_resized" {
+resource "libvirt_volume" "ubuntu2004_resized" {
   name           = "ubuntu-volume-${count.index}"
-  base_volume_id = libvirt_volume.ubuntu1804_cloud.id
+  base_volume_id = libvirt_volume.ubuntu2004_cloud.id
   pool           = "default"
   size           = 42949672960
   count          = 3
@@ -48,9 +58,8 @@ resource "libvirt_network" "kube_network" {
   }
 }
 
-
-resource "libvirt_domain" "k8s-master" {
-  name   = "k8s-master"
+resource "libvirt_domain" "k8s-controlplane" {
+  name   = "k8s-controlplane"
   memory = "4096"
   vcpu   = 2
 
@@ -58,13 +67,13 @@ resource "libvirt_domain" "k8s-master" {
   
   network_interface {
     network_id     = libvirt_network.kube_network.id
-    hostname       = "k8s-master"
+    hostname       = "k8s-controlplane"
     addresses      = ["172.16.1.11"]
     wait_for_lease = true
   }
 
   disk {
-    volume_id = libvirt_volume.ubuntu1804_resized[0].id
+    volume_id = libvirt_volume.ubuntu2004_resized[0].id
   }
 
   console {
@@ -80,12 +89,12 @@ resource "libvirt_domain" "k8s-master" {
   }
 }
 
-output "ip-master" {
-  value = libvirt_domain.k8s-master.network_interface[0].addresses[0]
+output "ip-controlplane" {
+  value = libvirt_domain.k8s-controlplane.network_interface[0].addresses[0]
 }
 
-resource "libvirt_domain" "k8s-worker-1" {
-  name   = "k8s-worker-1"
+resource "libvirt_domain" "k8s-node-1" {
+  name   = "k8s-node-1"
   memory = "2048"
   vcpu   = 2
 
@@ -93,13 +102,13 @@ resource "libvirt_domain" "k8s-worker-1" {
   
   network_interface {
     network_id     = libvirt_network.kube_network.id
-    hostname       = "k8s-worker-1"
+    hostname       = "k8s-node-1"
     addresses      = ["172.16.1.21"]
     wait_for_lease = true
   }
 
   disk {
-    volume_id = libvirt_volume.ubuntu1804_resized[1].id
+    volume_id = libvirt_volume.ubuntu2004_resized[1].id
   }
 
   console {
@@ -115,12 +124,12 @@ resource "libvirt_domain" "k8s-worker-1" {
   }
 }
 
-output "ip-worker-1" {
-  value = libvirt_domain.k8s-worker-1.network_interface[0].addresses[0]
+output "ip-node-1" {
+  value = libvirt_domain.k8s-node-1.network_interface[0].addresses[0]
 }
 
-resource "libvirt_domain" "k8s-worker-2" {
-  name   = "k8s-worker-2"
+resource "libvirt_domain" "k8s-node-2" {
+  name   = "k8s-node-2"
   memory = "2048"
   vcpu   = 2
 
@@ -128,13 +137,13 @@ resource "libvirt_domain" "k8s-worker-2" {
   
   network_interface {
     network_id     = libvirt_network.kube_network.id
-    hostname       = "k8s-worker-2"
+    hostname       = "k8s-node-2"
     addresses      = ["172.16.1.22"]
     wait_for_lease = true
   }
 
   disk {
-    volume_id = libvirt_volume.ubuntu1804_resized[2].id
+    volume_id = libvirt_volume.ubuntu2004_resized[2].id
   }
 
   console {
@@ -150,10 +159,10 @@ resource "libvirt_domain" "k8s-worker-2" {
   }
 }
 
-output "ip-worker-2" {
-  value = libvirt_domain.k8s-worker-2.network_interface[0].addresses[0]
+output "ip-node-2" {
+  value = libvirt_domain.k8s-node-2.network_interface[0].addresses[0]
 }
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.15"
 }
